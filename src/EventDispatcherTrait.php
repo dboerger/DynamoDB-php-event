67f42b00383cfbfc3b1eae495e4eed041a42634f
Minhao Zhang <minhao.zhang@qq.com>
2017-01-23 17:27:02 +0800

    * fix: php notice warning

--- src/EventDispatcherTrait.php
+++ src/EventDispatcherTrait.php
@@ -21,17 +21,17 @@ trait EventDispatcherTrait
     protected $eventListeners = [];
     /** @var EventDispatcherInterface */
     protected $delegateDispatcher = null;
-
+    
     public function getParentEventDispatcher()
     {
         return $this->eventParent;
     }
-
+    
     public function setParentEventDispatcher(EventDispatcherInterface $parent)
     {
         $this->eventParent = $parent;
     }
-
+    
     public function dispatch($event, $context = null)
     {
         if (!$event instanceof Event) {
@@ -40,67 +40,69 @@ trait EventDispatcherTrait
         if ($context) {
             $event->setContext($context);
         }
-
+        
         if ($this->delegateDispatcher instanceof EventDispatcherInterface) {
             $this->delegateDispatcher->dispatch($event);
-
+            
             return;
         }
-
+        
         /** @noinspection PhpParamsInspection */
         $event->setTarget($this);
-
+        
         $chain = [];
         /** @var EventDispatcherInterface $dispatcher */
         $dispatcher = $this;
         do {
             $chain[] = $dispatcher;
         } while ($dispatcher = $dispatcher->getParentEventDispatcher());
-
+        
         if (!$event->doesBubble()) { // this event uses capturing method
             $chain = array_reverse($chain);
         }
-
+        
         foreach ($chain as $dispatcher) {
             /** @noinspection PhpUndefinedMethodInspection */
             $dispatcher->doDispatchEvent($event);
-
+            
             if ($event->isPropogationStopped()) {
                 break;
             }
         }
     }
-
+    
     public function addEventListener($name, callable $listener, $priority = 0)
     {
-        if (!is_array($this->eventListeners[$name])) {
+        if (!isset($this->eventListeners[$name]) || !is_array($this->eventListeners[$name])) {
             $this->eventListeners[$name] = [];
         }
-        if (!is_array($this->eventListeners[$name][$priority])) {
+        if (!isset($this->eventListeners[$name][$priority])
+            || !is_array($this->eventListeners[$name][$priority])
+        ) {
             $this->eventListeners[$name][$priority] = [];
             ksort($this->eventListeners[$name]);
         }
-
+        
         $this->eventListeners[$name][$priority][] = $listener;
     }
-
+    
     public function removeEventListener($name, callable $listener)
     {
         $comp = function ($a, $b) {
             if (is_string($a) && is_string($b) && $a == $b) {
                 return true;
             }
-
-            if (is_array($a) && is_array($b) && count($a) == count($b)) {
+            
+            if (is_array($a) && is_array($b) && count($a) == 2 && count($b) == 2) {
                 if ($a[0] == $b[0] && $a[1] == $b[1]) {
                     return true;
                 }
             }
-
+            
             return $a === $b;
         };
-
-        if (is_array($this->eventListeners[$name])) {
+        
+        if (isset($this->eventListeners[$name]) && is_array($this->eventListeners[$name])) {
             foreach ($this->eventListeners[$name] as $priority => &$list) {
                 $new_list = [];
                 foreach ($list as $callback) {
@@ -112,7 +114,7 @@ trait EventDispatcherTrait
             }
         }
     }
-
+    
     public function removeAllEventListeners($name = '')
     {
         foreach ($this->eventListeners as $eventName => &$list) {
@@ -121,7 +123,7 @@ trait EventDispatcherTrait
             }
         }
     }
-
+    
     /**
      * @param EventDispatcherInterface|null $delegateDispatcher
      */
@@ -129,17 +131,17 @@ trait EventDispatcherTrait
     {
         $this->delegateDispatcher = $delegateDispatcher;
     }
-
+    
     protected function doDispatchEvent(Event $event)
     {
         /** @noinspection PhpParamsInspection */
         $event->setCurrentTarget($this);
-
-        if ($this->eventListeners[$event->getName()]) {
+        
+        if (isset($this->eventListeners[$event->getName()])) {
             foreach ($this->eventListeners[$event->getName()] as $priority => $callbacks) {
                 foreach ($callbacks as $callback) {
                     call_user_func($callback, $event);
-
+                    
                     if ($event->isPropogationStoppedImmediately()) {
                         return;
                     }
@@ -147,5 +149,5 @@ trait EventDispatcherTrait
             }
         }
     }
-
+    
 }
